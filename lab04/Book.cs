c9d8b95849740cb004fce482c8bb27dc071aa5e0
Practice-KhongTen <59854810+Practice-KhongTen@users.noreply.github.com>
2020-06-09 08:21:01 +0700

    chanhoa

---
+++ lab04/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace session6
+{
+    class Book
+    {
+        string _bookName;
+        //float _price;
+
+        public string Print()
+        {
+            return _bookName;
+        }
+
+        public void Input(string bkName)
+        {
+            _bookName = bkName;
+        }
+    }
+}
