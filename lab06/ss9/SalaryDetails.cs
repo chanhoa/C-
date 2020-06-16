c74028197cf17bebfb743098d6f0238436556cdd
Practice-KhongTen <59854810+Practice-KhongTen@users.noreply.github.com>
2020-06-16 11:17:15 +0700

    chanhoa

---
+++ lab06/ss9/SalaryDetails.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7
+{
+    class SalaryDetails
+    {
+        private string _empName;
+        public string EmployeeName
+        {
+            get
+            {
+                return _empName;
+            }
+            set
+            {
+                _empName = value;
+            }
+        }
+        static void Main(string[] args)
+        {
+            SalaryDetails objSal = new SalaryDetails();
+            objSal.EmployeeName = "Patrick Johnson";
+            Console.WriteLine("Employee Name: " + objSal.EmployeeName);
+            Console.Read();
+        }
+    }
+}
